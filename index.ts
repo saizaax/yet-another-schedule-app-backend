65a83fb8a072e4cbed6b7f77f48684feb0691ae7
xaazias <flamesuspect@gmail.com>
2022-08-21 22:33:00 +0300

    Added Prisma schema & src folder structure.

--- index.ts
+++ index.ts
@@ -1,13 +1,22 @@
 import express, { Express, Request, Response } from "express"
+import { PrismaClient } from "@prisma/client"
+
 import dotenv from "dotenv"
 
 dotenv.config()
 
-const app: Express = express()
-const port = process.env.PORT || 3000
+const prisma = new PrismaClient()
+const app = express()
+
+const port = Number(process.env.PORT) || 3000
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server")
+})
+
+app.get("/groups", async (req: Request, res: Response) => {
+  const groups = await prisma.group.findMany()
+  res.json(groups)
 })
 
 app.listen(port, () => {
