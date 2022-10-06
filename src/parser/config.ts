fa2de30c23f12cd85f4248661f8fc7fc12bd39f0
saizaax <flamesuspect@gmail.com>
2022-10-06 17:34:08 +0300

    Changed parser-url to env variable.

--- src/parser/config.ts
+++ src/parser/config.ts
@@ -1,4 +1,4 @@
 import axios from "axios"
 
-export const BASE_URL = "http://localhost:5000/api"
+export const BASE_URL = process.env.PARSER_URL
 export const API = axios.create({ baseURL: BASE_URL })
